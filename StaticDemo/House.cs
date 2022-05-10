f295f9142656b011b4a6096d447ed4bf7114eb63
Jose <josepls.v@hotmail.com>
2022-05-10 12:08:43 -0700

    first commit

---
+++ StaticDemo/House.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticDemo
+{
+    public class House
+    {
+        public House()
+        {
+        }
+
+        public string PaintColor { get; set; }
+        public bool HasGarage { get; set; }
+        public static void TurnOnLights()
+        {
+            Console.WriteLine("House lights are on");
+        }
+    }
+    public static class Home
+    {
+        public static string PaintColor2 { get; set; }
+        public static bool HasGarage { get; set; }
+    }
+    
+}
---
+++ StaticDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StaticDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myHouse = new House();
+
+            myHouse.PaintColor = "Green";
+            myHouse.HasGarage = true;
+
+            var my2ndHouse = new House();
+            my2ndHouse.PaintColor = "Red";
+            my2ndHouse.HasGarage = false;
+
+            House.TurnOnLights();
+
+            //using static home I can just use home isntead of creating an object BUT all variables will be assigned to the class. Not the specific object.
+            Home.PaintColor2 = "Blue";
+            Home.HasGarage = true;
+        }
+    }
+}
